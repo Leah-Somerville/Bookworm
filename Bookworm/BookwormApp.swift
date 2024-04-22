34eca4a20db1852af745c5eb4e855340a63a83f9
Leah Somerville <leah.somerville4@gmail.com>
2024-04-22 10:09:55 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Leah Somerville on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
